985998d577852344b129e2afd29e9230d3bb6b58
João Marcelo Uchôa de Alencar <jmarcelo.alencar@gmail.com>
2018-06-14 19:00:05 -0300

    Usando template para criar um bucket.

---
+++ aws/s3/bucketcreate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VPCID=$(aws ec2 describe-vpcs --filter "Name=isDefault, Values=true" --query "Vpcs[0].VpcId" --output text)
+SUBNETID=$(aws ec2 describe-subnets --filter "Name=vpc-id, Values=$VPCID" --query "Subnets[0].SubnetId" --output text)
+
+aws cloudformation create-stack --stack-name bucketcreate \
+--template-body file://bucketcreate.json \
+ --capabilities CAPABILITY_IAM \
+--parameters \
+ParameterKey=KeyName,ParameterValue=computacaoemnuvem \
+ParameterKey=VPC,ParameterValue=$VPCID \
+ParameterKey=Subnet,ParameterValue=$SUBNETID

ca23d02533da4d1c0fe9f9e37261c68d09c797b0
jmhalqxufc <jmhal@quixada.ufc.br>
2019-04-09 17:47:48 -0300

    Scripts para configurar o NFS.

--- aws/cluster/master.sh
+++ aws/cluster/master.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 # Master Node
 
-# Configure Internet Access for nodes
-sysctl -w net.ipv4.ip_forward=1
-
 # Install NFS
 apt update
 apt -y install nfs-kernel-server
-echo "/home       10.0.0.0/24(rw,sync,no_root_squash,no_subtree_check)" > /etc/exports
+echo "/home       10.0.0.0/16(rw,sync,no_root_squash,no_subtree_check)" > /etc/exports
 systemctl restart nfs-kernel-server
 
 # Create SSH Keys
 ssh-keygen -t dsa  -f /home/ubuntu/.ssh/id_dsa -N ""
 cat /home/ubuntu/.ssh/id_dsa.pub >> /home/ubuntu/.ssh/authorized_keys
 chown -R ubuntu.ubuntu /home/ubuntu/.ssh
+
+# Disable host verification
+cat <<EOF >> /etc/ssh/ssh_config
+Host 10.0.*
+   StrictHostKeyChecking no
+   UserKnownHostsFile=/dev/null
+   LogLevel ERROR
+EOF
 
 # Create hostfile
 echo "10.0.0.10" >> /home/hostfile
--- aws/cluster/worker.sh
+++ aws/cluster/worker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# NFSS Client Install
+# NFS Client Install
 
 # Install NFS
 apt update
 apt -y install nfs-common
-mount 10.0.010:/home /home
+mount -t 10.0.0.10:/home /home
 
 # Update hostfile
 ip -4 -br addr show dev eth0 | awk '{ print $3}' | cut -f1 -d'/' >> /home/hostfile

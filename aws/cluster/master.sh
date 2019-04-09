8d17e8251c295d2e2dec62597e27a249e7db92d7
jmhalqxufc <jmhal@quixada.ufc.br>
2019-04-09 18:17:46 -0300

    Consertei o tipo de chave de dsa para rsa.

--- aws/cluster/master.sh
+++ aws/cluster/master.sh
@@ -8,8 +8,8 @@ echo "/home       10.0.0.0/16(rw,sync,no_root_squash,no_subtree_check)" > /etc/e
 systemctl restart nfs-kernel-server
 
 # Create SSH Keys
-ssh-keygen -t dsa  -f /home/ubuntu/.ssh/id_dsa -N ""
-cat /home/ubuntu/.ssh/id_dsa.pub >> /home/ubuntu/.ssh/authorized_keys
+ssh-keygen -t rsa -f /home/ubuntu/.ssh/id_rsa -N ""
+cat /home/ubuntu/.ssh/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
 chown -R ubuntu.ubuntu /home/ubuntu/.ssh
 
 # Disable host verification
--- aws/cluster/worker.sh
+++ aws/cluster/worker.sh
@@ -4,7 +4,7 @@
 # Install NFS
 apt update
 apt -y install nfs-common
-mount -t 10.0.0.10:/home /home
+mount -t nfs 10.0.0.10:/home /home
 
 # Update hostfile
 ip -4 -br addr show dev eth0 | awk '{ print $3}' | cut -f1 -d'/' >> /home/hostfile

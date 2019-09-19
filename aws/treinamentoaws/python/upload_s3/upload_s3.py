6a7a5fc01e7ab4f20c247341af35a1554038f971
Ubuntu <ubuntu@ip-172-31-90-125.ec2.internal>
2019-09-19 13:43:59 +0000

    Upload de Arquivos no S3

---
+++ aws/treinamentoaws/python/upload_s3/upload_s3.py
@@ -0,0 +1,34 @@
+import boto3
+import os
+
+from flask import Flask, render_template, flash, redirect, request
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from wtforms.validators import DataRequired
+from werkzeug import secure_filename
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+s3_client = boto3.client('s3')
+
+class UploadForm(FlaskForm):
+    file = FileField('Arquivo:', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    form = UploadForm()
+    if request.method == 'POST':
+       f = request.files['file']
+
+
+       f.save(secure_filename(f.filename))
+       s3_client.upload_file(f.filename, 'treinamentoawsufcqx', f.filename)
+       os.remove(f.filename)
+
+       return redirect('/index')
+    return render_template('index.html', title='AWS Upload de Arquivo', form=form)
+
+
+

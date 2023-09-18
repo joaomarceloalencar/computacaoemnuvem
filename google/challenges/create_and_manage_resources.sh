755ba22cdc7e517868fdd44e981bed5c0e1aae21
joaomarceloalencar <joao.marcelo@ufc.br>
2023-09-18 19:38:35 -0300

    Funcionou!

--- google/challenges/create_and_manage_resources.sh
+++ google/challenges/create_and_manage_resources.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# https://cloud.google.com/iap/docs/load-balancer-howto?hl=pt-br#gcloud_1
+
 
 REGION=us-east1
 ZONE=us-east1-b
@@ -22,33 +22,40 @@ read -p "Verify and press enter..."
 CLUSTER="nucleus-cluster"
 PORT=8081
 gcloud container clusters create --machine-type=e2-medium --zone=$ZONE ${CLUSTER}
-gcloud container clusters get-credentials ${CLUSTER}
+gcloud container clusters get-credentials ${CLUSTER} --zone=$ZONE
 kubectl create deployment hello-server --image=gcr.io/google-samples/hello-app:2.0
 kubectl expose deployment hello-server --type=LoadBalancer --port $PORT
 
 read -p "Verify and press enter..."
 
 # Task 3. Set up an HTTP load balancer
+# https://cloud.google.com/iap/docs/load-balancer-howto?hl=pt-br#gcloud_1
 FIREWALL_RULE=lb-firewall-rule
 
 ## Create an instance template.
 gcloud compute instance-templates create lb-backend-template \
-   --region=${REGION} \
-   --network=default \
-   --subnet=default \
-   --tags=allow-health-check \
-   --machine-type=e2-micro \
-   --image-family=debian-11 \
-   --image-project=debian-cloud \
-   --metadata-from-file=startup-script=./startup.sh
+  --region=${REGION} \
+  --network=default \
+  --subnet=default \
+  --tags=allow-health-check \
+  --machine-type=e2-micro \
+  --image-family=debian-10 \
+  --image-project=debian-cloud \
+  --metadata-from-file=startup-script=./startup.sh
 
-## Create a target pool.
-gcloud compute target-pools create www-pool \
-  --region ${REGION} 
+## Create a target pool. (I think is not necessary)
+#gcloud compute target-pools create www-pool \
+#  --region ${REGION} 
 
 ## Create a managed instance group.
-gcloud compute instance-groups managed create lb-backend-group \
-   --template=lb-backend-template --size=2 --zone=${ZONE}
+gcloud compute instance-groups managed create lb-backend-example \
+  --template=lb-backend-template \
+  --size=2 \
+  --zone=${ZONE}
+
+gcloud compute instance-groups set-named-ports lb-backend-example \
+  --named-ports http:80 \
+  --zone=${ZONE}
 
 ## Create a firewall rule named as Firewall rule to allow traffic (80/tcp).
 gcloud compute firewall-rules create ${FIREWALL_RULE} \
@@ -59,39 +66,45 @@ gcloud compute firewall-rules create ${FIREWALL_RULE} \
   --target-tags=allow-health-check \
   --rules=tcp:80
 
+## Allocate IP
+gcloud compute addresses create lb-ipv4-1 \
+  --ip-version=IPV4 \
+  --network-tier=PREMIUM \
+  --global
+
+IP=$(gcloud compute addresses describe lb-ipv4-1 --format="get(address)" --global)
+
 ## Create a health check.
 gcloud compute health-checks create http http-basic-check \
   --port 80
 
 ## Create a backend service, and attach the managed instance group with named port (http:80).
 gcloud compute backend-services create web-backend-service \
+  --load-balancing-scheme=EXTERNAL \
   --protocol=HTTP \
   --port-name=http \
   --health-checks=http-basic-check \
   --global
 
 gcloud compute backend-services add-backend web-backend-service \
-  --instance-group=lb-backend-group \
+  --instance-group=lb-backend-example \
   --instance-group-zone=${ZONE} \
-  --global  
+  --global
 
 ## Create a URL map, and target the HTTP proxy to route requests to your URL map.
 gcloud compute url-maps create web-map-http \
-    --default-service web-backend-service
+  --default-service web-backend-service
 
 gcloud compute target-http-proxies create http-lb-proxy \
-    --url-map web-map-http
+  --url-map web-map-http
 
 ## Create a forwarding rule.
-gcloud compute addresses create lb-ipv4-1 \
-  --ip-version=IPV4 \
-  --global
-
 gcloud compute forwarding-rules create http-content-rule \
-   --address=lb-ipv4-1\
-   --global \
-   --target-http-proxy=http-lb-proxy \
-   --ports=80
+  --load-balancing-scheme=EXTERNAL \
+  --address=lb-ipv4-1 \
+  --global \
+  --target-http-proxy=http-lb-proxy \
+  --ports=80
 
 # Clean
 read -p "Press to clean the resources..."
